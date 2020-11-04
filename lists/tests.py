c259a064aa11ab52ac0f35667b8b579d48c247a1
Matthew Round <m@ttround.com>
2020-11-04 15:11:38 +0000

    To-Do can be added to table via POST request

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
+import re
 
 class HomePageTest(TestCase):
 
@@ -14,8 +15,31 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(
+            self.remove_csfr_token(response.content.decode()),
+            self.remove_csfr_token(expected_html)
+        )
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'},
+            request=request
+        )
+        self.assertEqual(
+            self.remove_csfr_token(response.content.decode()),
+            self.remove_csfr_token(expected_html)
+        )
+
+
+    def remove_csfr_token(self, html):
+        return re.sub('<input type="hidden" name="csrfmiddlewaretoken".+">', '', html)
 
 
 
