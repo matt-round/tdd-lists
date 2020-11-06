5112939e5f8a8257237a8530971fe65969c20463
Matthew Round <m@ttround.com>
2020-11-06 14:11:56 +0000

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -5,8 +5,12 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 import re
+from lists.models import Item
 
 class HomePageTest(TestCase):
+
+    def remove_csfr_token(self, html):
+        return re.sub('<input type="hidden" name="csrfmiddlewaretoken".+">', '', html)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -37,9 +41,25 @@ class HomePageTest(TestCase):
             self.remove_csfr_token(expected_html)
         )
 
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
 
-    def remove_csfr_token(self, html):
-        return re.sub('<input type="hidden" name="csrfmiddlewaretoken".+">', '', html)
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
+    
 
 
 
